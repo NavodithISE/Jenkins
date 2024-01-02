26ea53252ae405f107a99a5f2d24c63018183f68
NavodithISE <152998416+NavodithISE@users.noreply.github.com>
2024-01-02 08:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-app=Flask(__name__),template_folder='templates')
+app=Flask(__name__,template_folder='templates')
 
 @app.route("/")
 def home_page(name=None):
